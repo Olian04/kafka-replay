07c3279bb673a1221a33f5f008ff74098962005d
Oliver Linnarsson <oliver@lolo.company>
2026-02-03 16:07:14 +0100

    chore: Refactor

--- cmd/kafka-replay/main.go
+++ cmd/kafka-replay/main.go
@@ -2,33 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
+	
 	"log"
 	"os"
-	"strings"
+	
 
 	"github.com/urfave/cli/v3"
 )
-
-// resolveBrokers resolves broker addresses from command flags or environment variable.
-// Returns the broker addresses and an error if neither is provided.
-func resolveBrokers(flagBrokers []string) ([]string, error) {
-	if len(flagBrokers) > 0 {
-		return flagBrokers, nil
-	}
-
-	envBrokers := os.Getenv("KAFKA_BROKERS")
-	if envBrokers == "" {
-		return nil, fmt.Errorf("broker address(es) must be provided via --broker flag or KAFKA_BROKERS environment variable")
-	}
-
-	brokers := strings.Split(envBrokers, ",")
-	// Trim whitespace from each broker
-	for i, broker := range brokers {
-		brokers[i] = strings.TrimSpace(broker)
-	}
-	return brokers, nil
-}
 
 func main() {
 	app := &cli.Command{
